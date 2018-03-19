0275f0fd5793b4ca7c7a5b0a94aadbbac79c6ceb
AmyMish98 <31150661+AmyMish98@users.noreply.github.com>
2018-03-19 15:37:14 +0530

    Update Lab6_Prog2.cpp

--- Lab6_Prog2.cpp
+++ Lab6_Prog2.cpp
@@ -41,3 +41,57 @@ int main() {
     obj.display();
     return 0;
 }
+
+
+//BINARY OPERATOR
+#include<iostream>
+using namespace std;
+
+class complex {
+    int a, b;
+public:
+
+    void getvalue() {
+        cout << "\n Enter the value of Complex Numbers a,b: ";
+        cin >> a>>b;
+    }
+
+    complex operator+(complex ob) {
+        complex t;
+        t.a = a + ob.a;
+        t.b = b + ob.b;
+        return (t);
+    }
+
+    complex operator-(complex ob) {
+        complex t;
+        t.a = a - ob.a;
+        t.b = b - ob.b;
+        return (t);
+    }
+
+    void display() {
+        cout << a << " + " << b << "i" << "\n";
+    }
+};
+
+int main() {
+
+    complex obj1, obj2, result, result1;
+
+    obj1.getvalue();
+    obj2.getvalue();
+
+    result = obj1 + obj2;
+    result1 = obj1 - obj2;
+
+    cout << "Input Values:\n";
+    obj1.display();
+    obj2.display();
+
+    cout << "Result: \n";
+    result.display();
+    result1.display();
+
+    return 0;
+}
